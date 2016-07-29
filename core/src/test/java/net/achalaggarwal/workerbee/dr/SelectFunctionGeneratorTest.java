bf4e963cf2acb31a3b1eae4033e623b6e4d6f713
meetchandan <chandan00104@gmail.com>
2016-07-29 12:05:29 +0530

    Added tests for MaxSF and MinSF

--- core/src/test/java/net/achalaggarwal/workerbee/dr/SelectFunctionGeneratorTest.java
+++ core/src/test/java/net/achalaggarwal/workerbee/dr/SelectFunctionGeneratorTest.java
@@ -2,7 +2,9 @@ package net.achalaggarwal.workerbee.dr;
 
 import net.achalaggarwal.workerbee.Column;
 import net.achalaggarwal.workerbee.dr.selectfunction.AllStarSF;
+import net.achalaggarwal.workerbee.dr.selectfunction.MaxSF;
 import net.achalaggarwal.workerbee.dr.selectfunction.SubStrSF;
+import net.achalaggarwal.workerbee.dr.selectfunction.SumSF;
 import org.junit.Test;
 
 import static org.hamcrest.core.IsInstanceOf.instanceOf;
@@ -17,5 +19,15 @@ public class SelectFunctionGeneratorTest {
   @Test
   public void shouldReturnSunStrSFWhenSubstrIsUsed(){
     assertThat(SelectFunctionGenerator.substr(new Column(null, "", Column.Type.STRING), 1, 2), instanceOf(SubStrSF.class));
+  }
+
+  @Test
+  public void shouldReturnMaxSFWhenMaxIsUsed(){
+    assertThat(SelectFunctionGenerator.max(new Column(null, "", Column.Type.INT)), instanceOf(MaxSF.class));
+  }
+
+  @Test
+  public void shouldReturnSumSFWhenSumIsUsed(){
+    assertThat(SelectFunctionGenerator.sum(new Column(null, "", Column.Type.INT)), instanceOf(SumSF.class));
   }
 }
